b8edf176ced637d35078732284a02789fb565fba
TeeAmorn <theerut.amorn@gmail.com>
2023-03-29 04:54:47 -0500

    Added configuration for i3

---
+++ polybar/cuts/scripts/launcher.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+/home/teeamorn/.config/rofi/launchers/type-2/launcher.sh
+# rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/polybar/cuts/scripts/rofi/launcher.rasi
