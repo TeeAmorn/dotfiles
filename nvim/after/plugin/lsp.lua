2f05086b9b0ffff05e9bbc975199482778d5a439
TeeAmorn <theerut.amorn@gmail.com>
2023-04-23 12:32:36 -0500

    moved formatting related logic to formatter.lua

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -6,28 +6,6 @@ lsp.on_attach(function(_, bufnr)
         omit = { '<F2>', '<F3>', '<F4>' }
     })
 
-    -- format code in current buffer
-    vim.keymap.set({ 'n', 'x' }, '<leader>ff', function()
-        -- for the following filetypes, prettier is used instead
-        if vim.bo.filetype == "css" or
-            vim.bo.filetype == "graphql" or
-            vim.bo.filetype == "html" or
-            vim.bo.filetype == "javascript" or
-            vim.bo.filetype == "javascriptreact" or
-            vim.bo.filetype == "json" or
-            vim.bo.filetype == "less" or
-            vim.bo.filetype == "markdown" or
-            vim.bo.filetype == "scss" or
-            vim.bo.filetype == "typescript" or
-            vim.bo.filetype == "typescriptreact" or
-            vim.bo.filetype == "yaml"
-        then
-            vim.cmd('Prettier')
-        else
-            vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
-        end
-    end)
-
     -- rename all references to the symbol under the cursor
     vim.keymap.set({ 'n', 'x' }, '<leader>rr', "<cmd>lua vim.lsp.buf.rename()<CR>")
 
