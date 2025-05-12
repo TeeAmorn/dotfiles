5ee34dd6f9aa10bc9a280062faae81904e20e305
Tee Amornkasemwong <tamornkasemwong@netflix.com>
2025-05-12 08:41:07 -0700

    Update explorer options

--- lazyvim/lua/plugins/ui.lua
+++ lazyvim/lua/plugins/ui.lua
@@ -5,5 +5,19 @@ return {
     scratch = { enabled = false },
     terminal = { enabled = false },
     scroll = { enabled = false },
+    picker = {
+      sources = {
+        explorer = {
+          -- auto close explorer sidebar after selecting file
+          auto_close = true,
+        },
+        files = {
+          -- show hidden files when using fzf
+          hidden = true,
+        },
+      },
+      -- show hidden files in explorer
+      hidden = true,
+    },
   },
 }
