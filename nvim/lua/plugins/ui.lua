26b791d637642113e60abdc68823b057f95f64e6
Tee Amornkasemwong <tamornkasemwong@netflix.com>
2025-09-11 11:17:04 -0700

    add caddyfile support and enable flash, terminal, and scratch

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -2,8 +2,6 @@ return {
   "folke/snacks.nvim",
   opts = {
     dashboard = { enabled = false },
-    scratch = { enabled = false },
-    terminal = { enabled = false },
     scroll = { enabled = false },
     picker = {
       sources = {
